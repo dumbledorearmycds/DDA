1286483335cbdb427d615050544a84d87b021e2f
dumbledorearmycds <dumbledorearmycds@gmail.com>
2026-09-25 12:47:43 +0530

    feat(hoc): update continue life costs to 400 for 4th life and 800 for 5th life

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dda-hub-v8';
+const CACHE_NAME = 'dda-hub-v9';
 
 // Essential static assets to pre-cache on install
 // Note: All CSS and JavaScript for the application are self-contained within index.html.
